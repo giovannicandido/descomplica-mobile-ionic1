df5344c2546af5cb83b3e3ceda4da4ad59aa78d3
Giovanni Candido da Silva <915892+giovannicandido@users.noreply.github.com>
2023-04-23 17:39:37 -0300

    Adicionado android e geolocalizacao

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Geolocation, PermissionStatus } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tab3',
@@ -9,6 +10,85 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+
+  latitude: number | undefined = 0.0
+  longitude: number | undefined = 0.0
+  permission: PermissionStatus | undefined
+
+  watchId: any;
+
+  options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  };
+
+  constructor(private readonly toastController: ToastController, private readonly zone: NgZone) { }
+  
+  ngOnInit() {
+    this.requestPermissions()
+  }
+  async requestPermissions() {
+    const permResult = await Geolocation.requestPermissions();
+    console.log('Perm request result: ', permResult);
+    this.permission = permResult;
+  }
+
+  async getCurrentPosition() {
+    
+    const toast = await this.toastController.create({
+      message: `Getting current position`,
+      position: 'top',
+      duration: 3000
+    })
+    await toast.present()
+    
+    Geolocation.getCurrentPosition(this.options).then(data => {
+      const toast = this.toastController.create({
+        message: `Current position ${JSON.stringify(data)}`,
+        duration: 3000
+      }).then(t => t.present())
+      this.latitude = data.coords.altitude ? data.coords.latitude : undefined
+      this.longitude = data.coords.longitude ? data.coords.longitude : undefined
+    }).catch(err => {
+      const toast = this.toastController.create({
+        message: `Error: ${err}`,
+        duration: 3000
+      }).then(t => t.present().then(()=> {}))
+    });
+
+  }
+
+  watchPosition() {
+    if(this.watchId) {
+      return;
+    }
+
+    const watchId = Geolocation.watchPosition(this.options, async(coordinates, err) => {
+      this.zone.run(() => {
+        this.latitude = coordinates?.coords.latitude
+        this.longitude = coordinates?.coords.longitude
+      })
+
+      if (err) {
+        const toast = await this.toastController.create({
+          message: `Error: ${err}`,
+          duration: 3000
+        })
+
+        await toast.present()
+
+      }
+    })
+
+    this.watchId = watchId
+
+  }
+
+  clearWatch() {
+    if (this.watchId) {
+      Geolocation.clearWatch({ id: this.watchId });
+      this.watchId = null;
+    }
+  }
 }
